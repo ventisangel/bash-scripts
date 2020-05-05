817b471ce9851d88122303537a1804df9cfdd49c
Ventisangel <62400268+ventisangel@users.noreply.github.com>
2020-05-05 18:50:53 +0200

    Added some more applications to choose from

--- appssetup.sh
+++ appssetup.sh
@@ -26,6 +26,23 @@ function addRepo {
     fi
 }
 
+function addGoogleRepoKey {
+    read -rp "You need to download and install appropriate signing key in order to install packages from Google Linux Software Repositories. Type 'y' if you want to do it now, or 'n' if you have already done that: " answer
+    if [[ "$answer" = "y" ]]; then
+        echo "Downloading and installing Google Linux Software Repositories package signing key..."
+        wget -O google_linux_signing_key.pub https://dl.google.com/linux/linux_signing_key.pub
+        case $distro in
+            debian|ubuntu)
+                sudo apt-key add google_linux_signing_key.pub
+            ;;
+            fedora|opensuse-leap|opensuse-tumbleweed)
+                sudo rpm --import google_linux_signing_key.pub
+            ;;
+        esac
+        unset answer
+    fi
+}
+
 function terminate {
     cd ..
     echo "Removing temporary directory..."
@@ -40,7 +57,7 @@ echo "Entering created directory..."
 cd /tmp/tempDir || exit
 
 distro=$(grep -oP '(?<=^ID=).+' /etc/os-release | tr -d '"')
-menuitems=( 'Discord' 'Skype' 'VS Code' 'VS Code Insiders' 'Telegram' 'Lexmark Printer Driver' 'Terminate script' )
+menuitems=( 'Discord' 'Skype' 'VS Code' 'VS Code Insiders' 'Telegram' 'MS Teams' 'Google Chrome' 'Google Earth' 'Opera' 'Lexmark Printer Driver' 'Terminate script' )
 PS3="Which of these apps/drivers do you want to install? "
 
 trap "echo You cannot interrupt execution of this script, please wait..." SIGINT SIGQUIT SIGTERM
@@ -71,6 +88,24 @@ do
                         addRepo "Universe" "Telegram"
                     fi
                     sudo apt-get install telegram-desktop
+                ;;
+                'MS Teams')
+                    wget -O ms-teams.deb https://go.microsoft.com/fwlink/p/\?linkid=2112886
+                    sudo apt-get install ./ms-teams.deb
+                ;;
+                'Google Chrome')
+                    addGoogleRepoKey
+                    wget -O google-chrome.deb https://dl.google.com/linux/direct/google-chrome-stable_current_amd64.deb
+                    sudo apt-get install ./google-chrome.deb
+                ;;
+                'Google Earth')
+                    addGoogleRepoKey
+                    wget -O google-earth.deb http://dl.google.com/dl/earth/client/current/google-earth-pro-stable_current_amd64.deb
+                    sudo apt-get install ./google-earth.deb
+                ;;
+                'Opera')
+                    wget -O opera.deb https://download.opera.com/download/get/\?partner=www\&opsys=Linux\&package=DEB
+                    sudo apt-get install ./opera.deb
                 ;;
                 #'Lexmark Printer Driver')
                     # .deb package in preparation...
@@ -121,10 +156,44 @@ do
                 'Telegram')
                     if [[ "$distro" = "fedora" ]]; then
                         addRepo "RPMFusion" "Telegram"
-                        sudo dnf install telegram
+                        sudo dnf install telegram-desktop
                     else
                         addRepo "Non-OSS" "Telegram"
-                        sudo zypper install telegram
+                        sudo zypper install telegram-desktop
+                    fi
+                ;;
+                'MS Teams')
+                    wget -O ms-teams.rpm https://go.microsoft.com/fwlink/p/\?linkid=2112907
+                    if [[ "$distro" = "fedora" ]]; then
+                        sudo dnf install ./ms-teams.rpm
+                    else
+                        sudo zypper install ./ms-teams.rpm
+                    fi
+                ;;
+                'Google Chrome')
+                    addGoogleRepoKey
+                    wget -O google-chrome.rpm https://dl.google.com/linux/direct/google-chrome-stable_current_x86_64.rpm
+                    if [[ "$distro" = "fedora" ]]; then
+                        sudo dnf install ./google-chrome.rpm
+                    else
+                        sudo zypper install ./google-chrome.rpm
+                    fi
+                ;;
+                'Google Earth')
+                    addGoogleRepoKey
+                    wget -O google-earth.rpm http://dl.google.com/dl/earth/client/current/google-earth-pro-stable-current.x86_64.rpm
+                    if [[ "$distro" = "fedora" ]]; then
+                        sudo dnf install ./google-earth.rpm
+                    else
+                        sudo zypper install ./google-earth.rpm
+                    fi
+                ;;
+                'Opera')
+                    wget -O opera.rpm https://download.opera.com/download/get/\?partner=www\&opsys=Linux\&package=RPM
+                    if [[ "$distro" = "fedora" ]]; then
+                        sudo dnf install ./opera.rpm
+                    else
+                        sudo zypper install ./opera.rpm
                     fi
                 ;;
                 #'Lexmark Printer Driver')
